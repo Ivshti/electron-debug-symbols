4a09e274ef14867943a337304b073260cf361717
Ivo Georgiev <ivo@linvo.org>
2015-07-15 11:59:47 +0300

    added an entry point to require all symbols

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = {
+	paths: [__dirname+"/symbols/win32/electron.breakpad.syms", __dirname+"/symbols/darwin/electron.breakpad.syms", __dirname+"/symbols/linux/electron.breakpad.syms"]
+};
