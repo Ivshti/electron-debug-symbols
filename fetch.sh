8ce4a7c7e0014e193a9130b99cdac22396e44e58
Ivo Georgiev <ivo@linvo.org>
2015-07-15 11:57:05 +0300

    fetch script to download & extract symbols

--- fetch.sh
+++ fetch.sh
@@ -4,3 +4,8 @@ if [ ! $1 ]; then
 	echo "must provide electron version: usage $0 ELECTRON_VERSION"
 	exit 1
 fi
+
+mkdir -p symbols/{win32,darwin,linux}
+( cd symbols/win32 ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-win32-ia32-symbols.zip | bsdtar -xvf- )
+( cd symbols/darwin ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-linux-x64-symbols.zip | bsdtar -xvf- )
+( cd symbols/linux ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-darwin-x64-symbols.zip | bsdtar -xvf- )
