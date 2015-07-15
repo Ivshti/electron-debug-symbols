0dc17fa33ac7a25f2b4c80780c5b0a61148b6605
Ivo Georgiev <ivo@linvo.org>
2015-07-15 12:21:38 +0300

    use tar instead of bsdtar

--- fetch.sh
+++ fetch.sh
@@ -6,6 +6,6 @@ if [ ! $1 ]; then
 fi
 
 mkdir -p symbols/{win32,darwin,linux}
-( cd symbols/win32 ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-win32-ia32-symbols.zip | bsdtar -xvf- )
-( cd symbols/darwin ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-linux-x64-symbols.zip | bsdtar -xvf- )
-( cd symbols/linux ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-darwin-x64-symbols.zip | bsdtar -xvf- )
+( cd symbols/win32 ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-win32-ia32-symbols.zip | tar -xvf- )
+( cd symbols/darwin ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-linux-x64-symbols.zip | tar -xvf- )
+( cd symbols/linux ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-darwin-x64-symbols.zip | tar -xvf- )
