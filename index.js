5135841ac0c05ae5d026de4c9d303d82d5603859
Ivo Georgiev <ivo@linvo.org>
2015-07-16 22:35:05 +0300

    index.js: read paths dynamically

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-module.exports = {
-	paths: [__dirname+"/symbols/win32/electron.breakpad.syms", __dirname+"/symbols/darwin/electron.breakpad.syms", __dirname+"/symbols/linux/electron.breakpad.syms"]
-};
+var path = require("path");
+var fs = require("fs");
+
+var paths = [__dirname+"/symbols/win32/electron.breakpad.syms", __dirname+"/symbols/darwin/electron.breakpad.syms", __dirname+"/symbols/linux/electron.breakpad.syms"];
+try { 
+	paths = fs.readdirSync(__dirname+"/symbols").map(function(x) { return path.join(__dirname+"/symbols", x) });
+} catch(e) { }
+
+module.exports = { paths: paths };
