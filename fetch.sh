30a98e7720f30bb9918151b736393bb1f603226c
Ivo Georgiev <ivo@linvo.org>
2015-07-16 22:38:49 +0300

    fetch.sh: allow fetching multiple versions

--- fetch.sh
+++ fetch.sh
@@ -7,17 +7,22 @@ fi
 
 # TODO: allow installing debug symbols for multiple electron versions  through passing a var $ELECTRON_VERSIONS
 
-mkdir -p symbols/{win32,darwin,linux}
+for VERSION in "$@"
+do
 
-if [ `which bsdtar` ] && [ ! $FORCE_UNZIP ]; then 
-	cmd="bsdtar -xvf-"
-	( cd symbols/win32 ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-win32-ia32-symbols.zip | $cmd )
-	( cd symbols/darwin ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-darwin-x64-symbols.zip | $cmd )
-	( cd symbols/linux ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-linux-x64-symbols.zip | $cmd )
-elif [ `which unzip` ]; then
-	cmd="unzip -x"
-	( cd symbols/win32 ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-win32-ia32-symbols.zip > /tmp/win32.zip ; $cmd /tmp/win32.zip )
-	( cd symbols/darwin ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-darwin-x64-symbols.zip > /tmp/darwin.zip ; $cmd /tmp/darwin.zip )
-	( cd symbols/linux ; wget -O - https://github.com/atom/electron/releases/download/v0.29.2/electron-v${1}-linux-x64-symbols.zip > /tmp/linux.zip ; $cmd /tmp/linux.zip )	
-fi
+	mkdir -p symbols/{win32-$VERSION,darwin-$VERSION,linux-$VERSION}
 
+	if [ `which bsdtar` ] && [ ! $FORCE_UNZIP ]; then 
+		cmd="bsdtar -xvf-"
+		( cd symbols/win32 ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-ia32-symbols.zip | $cmd )
+		( cd symbols/darwin ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-darwin-x64-symbols.zip | $cmd )
+		( cd symbols/linux ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-linux-x64-symbols.zip | $cmd )
+	elif [ `which unzip` ]; then
+		cmd="unzip -x"
+		( cd symbols/win32 ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-ia32-symbols.zip > /tmp/win32.zip ; $cmd /tmp/win32.zip )
+		( cd symbols/darwin ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-darwin-x64-symbols.zip > /tmp/darwin.zip ; $cmd /tmp/darwin.zip )
+		( cd symbols/linux ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-linux-x64-symbols.zip > /tmp/linux.zip ; $cmd /tmp/linux.zip )	
+	fi
+
+
+done
